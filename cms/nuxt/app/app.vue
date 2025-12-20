f54f579f3292413657bc3cef97054ace51103887
karljazz74 <karlpallan@gmail.com>
2025-12-20 21:33:28 +0100

    feat: Update branding - Replace Directus logo with Mexihaus logo and favicon

--- cms/nuxt/app/app.vue
+++ cms/nuxt/app/app.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 useHead({
 	meta: [{ charset: 'utf-8' }, { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
-	link: [{ rel: 'icon', href: '/favicon.ico' }],
+	link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }],
 	htmlAttrs: { lang: 'en' },
 });
 </script>
