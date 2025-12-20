78cac97ea3acca42b172f54a0eeabc1d9b7ae7d5
karljazz74 <karlpallan@gmail.com>
2025-12-20 23:42:07 +0100

    fix: Use NUXT_PUBLIC_DIRECTUS_URL in runtimeConfig

--- cms/nuxt/nuxt.config.ts
+++ cms/nuxt/nuxt.config.ts
@@ -33,7 +33,7 @@ export default defineNuxtConfig({
 	runtimeConfig: {
 		public: {
 			siteUrl: process.env.NUXT_PUBLIC_SITE_URL as string,
-			directusUrl: process.env.DIRECTUS_URL as string,
+			directusUrl: process.env.NUXT_PUBLIC_DIRECTUS_URL as string,
 			enableVisualEditing: process.env.NUXT_PUBLIC_ENABLE_VISUAL_EDITING !== 'false',
 		},
 		directusServerToken: process.env.DIRECTUS_SERVER_TOKEN,
